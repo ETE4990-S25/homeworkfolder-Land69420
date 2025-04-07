408af2a718c452a2b2fcc73b5b8de412e0036c87
Alexander-Land <76393855+Alexander-Land@users.noreply.github.com>
2025-04-07 13:14:01 -0700

    finished multiprocessing part of assignment

--- Project 2/Multiprocessing_prime_fibo_factorial.py
+++ Project 2/Multiprocessing_prime_fibo_factorial.py
@@ -1,4 +1,5 @@
-import threading
+import multiprocessing
+import multiprocessing.process
 import time
 
 # large prime part of project
@@ -23,6 +24,7 @@ def primes_in_range(start, stepsize, result, time_limit):
 
     result.append(highest_prime)
 
+
 def highest_prime(time_limit, number_of_threads):
     """each thread will find if the n*number of threads + i number is prime where 
     n is the iteration and i will be range(0, number of threads)
@@ -32,25 +34,25 @@ def highest_prime(time_limit, number_of_threads):
 
     print(f"finding largest prime in {time_limit} seconds.")
     
-    shared_result = [-1]
+    manager = multiprocessing.Manager()
+    shared_list = manager.list()
 
-    threads = [
-        threading.Thread(
+    Processes = [
+        multiprocessing.Process(
             target = primes_in_range,
-            args = (2*i+1 , number_of_threads * 2, shared_result, time_limit)
+            args = (2*i+1 , number_of_threads * 2, shared_list, time_limit)
         )
         for i in range(number_of_threads)
     ]
 
-    for t in threads:
-        t.start()
+    for p in Processes:
+        p.start()
  
 
-    for t in threads:
-        t.join()
+    for p in Processes:
+        p.join()
 
-    return max(shared_result) if shared_result else "failed"
-
+    return max(shared_list) if shared_list else "failed"
 
 
 # fibonacci part of project
@@ -65,47 +67,42 @@ def nth_fibo(n):
 
 # factorial part of project
 def factorial_part(start, end, shared_result, lock):
+    """this is ur code from lecture"""
     result = 1
+
     for i in range(start, end):
         result *= i
 
     with lock:
-        shared_result[0] *= result
+        shared_result.value *= result
 
-def factorial(n , number_of_threads):
-    range_per_process = n // number_of_threads
-    shared_result = [1.0]
-    lock = threading.Lock()
+def factorial(n, number_of_processes):
+    range_per_process = n // number_of_processes
+    shared_result = multiprocessing.Value('d', 1.0) 
+    lock = multiprocessing.Lock()
 
-    threads = [
-        threading.Thread(
-            target = factorial_part,
-            args = (i * range_per_process + 1, (i + 1) * range_per_process + 1, shared_result, lock)
-        )
-        for i in range(number_of_threads)
-    ] 
+    processes = [
+        multiprocessing.Process(
+            target=factorial_part, 
+            args=(i * range_per_process + 1, (i + 1) * range_per_process + 1, shared_result, lock))
+            for i in range(number_of_processes)
+    ]
 
-    for t in threads:
-        t.start()
- 
 
-    for t in threads:
-        t.join()
-    
-    return (shared_result[0])
-    
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
 
+    return(shared_result.value)
 
 if __name__ == "__main__":
 
-    hp = highest_prime(time_limit= 0.5, number_of_threads= 10)
+    hp = highest_prime(time_limit= 1, number_of_threads= 10)
     print(hp)
 
-    high_fibo = nth_fibo(hp)
+    high_fibo = nth_fibo(10)
     print(high_fibo)
 
-    high_factorial = factorial(hp, 10)
+    high_factorial = factorial(10, 10)
     print(high_factorial)
-
-
-
