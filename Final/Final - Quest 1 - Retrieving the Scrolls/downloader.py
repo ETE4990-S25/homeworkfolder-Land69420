158be45bdf86ecaf4f902b7ea5a085e21ada8109
Alexander-Land <76393855+Alexander-Land@users.noreply.github.com>
2025-05-10 20:28:16 -0700

    setting up finall

---
+++ Final/Final - Quest 1 - Retrieving the Scrolls/downloader.py
@@ -0,0 +1,4 @@
+
+
+rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"]
+ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
